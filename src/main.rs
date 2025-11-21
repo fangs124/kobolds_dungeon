c7db7c2db3545319efc2756359ac50a079a79ff3
Fangs <fangs124@users.noreply.github.com>
2025-11-21 17:47:48 +0700

    commit

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
 use bevy::prelude::*;
 
 fn main() {
-    App::new().run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup_camera)
+        .run();
+}
+
+fn setup_camera(mut commands: Commands) {
+    commands.spawn(Camera2d);
+}
+
+fn load_textures(mut commands: Commands, asset_server: Res<AssetServer>) {
+    //commands.insert_resource();
 }
