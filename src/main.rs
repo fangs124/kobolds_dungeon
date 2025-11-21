c5301966d8e4a8c45cd95100fc2af758e1809109
Fangs <fangs124@users.noreply.github.com>
2025-11-21 18:49:29 +0700

    commit

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,33 @@
-use bevy::prelude::*;
+use bevy::{
+    prelude::*,
+    sprite_render::{TileData, TilemapChunk, TilemapChunkTileData},
+};
+use rand::{Rng, SeedableRng, random_range};
+use rand_chacha::ChaCha8Rng;
 
 fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins)
-        .add_systems(Startup, setup_camera)
-        .run();
+    App::new().add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest())).add_systems(Startup, setup).run();
 }
 
-fn setup_camera(mut commands: Commands) {
+#[derive(Component, Deref, DerefMut)]
+struct UpdateTimer(Timer);
+
+#[derive(Resource, Deref, DerefMut)]
+struct SeededRng(ChaCha8Rng);
+fn setup(mut commands: Commands, assets: Res<AssetServer>) {
+    let mut rng = ChaCha8Rng::seed_from_u64(42);
+    let chunk_size = UVec2::new(64, 64);
+    let tile_display_size = UVec2::new(8, 8);
+    let tile_data: Vec<Option<TileData>> =
+        (0..chunk_size.element_product()).map(|_| random_range(0..5)).map(|i| if i == 0 { None } else { Some(TileData::from_tileset_index(i - 1)) }).collect();
+
+    commands.spawn((
+        TilemapChunk { chunk_size, tile_display_size, tileset: assets.load("spritesheet.png"), ..default() },
+        TilemapChunkTileData(tile_data),
+        UpdateTimer(Timer::from_seconds(0.1, TimerMode::Repeating)),
+    ));
+
     commands.spawn(Camera2d);
-}
 
-fn load_textures(mut commands: Commands, asset_server: Res<AssetServer>) {
-    //commands.insert_resource();
+    commands.insert_resource(SeededRng(rng));
 }
